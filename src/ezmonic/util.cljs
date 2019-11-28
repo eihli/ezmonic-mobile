99c76fdf781bb73d8fc9eee10071a20434d8f7aa
Александар Симић <asimic@gmail.com>
2019-11-28 15:19:28 +0000

    Added conversion of number to mnemonics as a map
    At first I added these functions as I copied util namespace from the
    web app wholesale.
    
    Then I removed them, as they weren't used. But they weren't used
    because I wasn't mapping the numbers back to the group of mnemonics
    returned. But now since I need to do that, I've added them back in.
    
    I've also introduced number->mnemonics function so that I can get the
    "chunks" of the input number as keys and the corresponding mnemonics
    as values, as a map.

--- src/ezmonic/util.cljs
+++ src/ezmonic/util.cljs
@@ -72,3 +72,120 @@
     (fn [phrase]
       ((complement some) nil? phrase))
     (all-phrases number))))
+
+
+(def phones '(["AA" "vowel"]
+              ["AE" "vowel"]
+              ["AH" "vowel"]
+              ["AO" "vowel"]
+              ["AW" "vowel"]
+              ["AY" "vowel"]
+              ["B" "stop"]
+              ["CH" "affricate"]
+              ["D" "stop"]
+              ["DH" "fricative"]
+              ["EH" "vowel"]
+              ["ER" "vowel"]
+              ["EY" "vowel"]
+              ["F" "fricative"]
+              ["G" "stop"]
+              ["HH" "aspirate"]
+              ["IH" "vowel"]
+              ["IY" "vowel"]
+              ["JH" "affricate"]
+              ["K" "stop"]
+              ["L" "liquid"]
+              ["M" "nasal"]
+              ["N" "nasal"]
+              ["NG" "nasal"]
+              ["OW" "vowel"]
+              ["OY" "vowel"]
+              ["P" "stop"]
+              ["R" "liquid"]
+              ["S" "fricative"]
+              ["SH" "fricative"]
+              ["T" "stop"]
+              ["TH" "fricative"]
+              ["UH" "vowel"]
+              ["UW" "vowel"]
+              ["V" "fricative"]
+              ["W" "semivowel"]
+              ["Y" "semivowel"]
+              ["Z" "fricative"]
+              ["ZH" "fricative"]))
+
+
+(def not-really-vowels #{"ER" "W"})
+
+
+(def consonants
+  "Every phone that we don't consider to be a vowel.
+   These are the sounds that we need to map to numbers."
+  (clojure.set/union
+   not-really-vowels
+   (into #{} (map first (filter #(and
+                                  (not= (second %) "vowel")
+                                  (not= (second %) "semivowel"))
+                                phones)))))
+
+
+(defn strip-vowels
+  [phones]
+  (filter #(consonants %1) phones))
+
+
+(def consonant-number-map
+  "Used to map words from the CMU pronouncing dictionary to numbers.
+   For the entry `OVERWORK  OW2 V ER0 W ER1 K`:
+   Ignore the `OW2` since it's not a consonant.
+   V         -> 8
+   ER0 -> ER -> 4  ;; The 0/1/2 at the end is the stress, we don't care about it.
+   W         -> 3
+   ER1 -> ER -> 4
+   K         -> 7"
+  {"T" 1
+   "CH" 6
+   "K" 7
+   "HH" 2
+   "L" 5
+   "JH" 6
+   "G" 7
+   "M" 3
+   "W" 3
+   "S" 0
+   "Z" 0
+   "R" 4
+   "ER" 4
+   "F" 8
+   "B" 9
+   "SH" 6
+   "P" 9
+   "V" 8
+   "TH" 1
+   "N" 2
+   "DH" 1
+   "ZH" 6
+   "NG" 7
+   "D" 1})
+
+
+(defn normalize-consonants
+  [consonants]
+  "Converts a sequence of consonants to a sequence of numbers."
+  (map consonant-number-map consonants))
+
+
+(defn number->mnemonics
+  "Given a `number`, return all the mnemonics, grouped by the numbers as
+  keys and mnemonics as values e.g. for 333 it returns:
+
+  ({33 [MAMA MEMO MIME MOM]}
+   {3 [AIM EMU WAY WHEY YAM]})"
+  [number]
+  (into {} (for [phone (all-mezmorizations number)]
+             (hash-map (->> phone
+                            first
+                            strip-vowels
+                            normalize-consonants
+                            (clojure.string/join ""))
+                       (mapv first phone)))))
