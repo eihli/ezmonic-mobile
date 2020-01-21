54ae2c971ff8b68051198dab9dd0c47a72f7da71
Eric Ihli <ericihli@gmail.com>
2020-01-21 09:25:38 -0800

    Add prod entrypoint

---
+++ src/ezmonic/prod.cljs
@@ -0,0 +1,20 @@
+(ns ezmonic.prod
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            ["react-native" :as rn]
+            [ezmonic.views.core :as views]
+            [ezmonic.subs]
+            [shadow.cljs.devtools.client.react-native :as shadow-rn]
+            [ezmonic.events]))
+
+(rf/dispatch-sync [:initialize-db])
+
+(defn start []
+  (rn/AppRegistry.registerComponent
+   "Ezmonic"
+   (fn [] (r/reactify-component views/app-container))))
+
+(defn ^:export init []
+  (start))
+
+
